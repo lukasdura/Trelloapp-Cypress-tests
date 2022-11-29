a3035ed0fe544422ff3aa43c58bb1ad3e964f9b3
lukasdura <xswatx@gmail.com>
2022-11-29 17:08:09 +0100

    changed names with numbers

---
+++ cypress/e2e/Short_example_tests/6_UI_login.js
@@ -0,0 +1,35 @@
+
+const email    = 'lukas@lukas.sk'
+const password = 'lukas'
+
+
+describe('Try to login with UI to page', ()=>{
+
+
+it('', ()=>{
+
+cy.visit('/')
+
+ cy.get('[data-cy="login-menu"]')
+  .click()
+    cy.get('[data-cy="login-email"]')
+     .click()
+      .type(email)
+       .type('{enter}')
+
+cy.get('[data-cy="login-password"]')      
+ .click('')
+  .type(password)
+   .type('{enter}')  
+
+
+cy.get('#loginMessage')
+ .should('be.visible')
+  .should('contain.text','User is logged in')
+
+
+cy.get('[data-cy="logged-user"]')
+ .should('contain.have.text', email)
+
+})
+})
